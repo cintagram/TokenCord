b382efc5b383939f6919d9990eb95b07303e7c38
[ CatKnife | チャンチャンパルス ] <72387902+cintagram@users.noreply.github.com>
2024-04-27 10:51:50 +0900

    Update distok_v2.py

--- hyper/distok_v2.py
+++ hyper/distok_v2.py
@@ -8,8 +8,6 @@ import sys
 import os
 from pystyle import Colors, Write
 import time
-from BCSFE_Python_Discord import *
-import BCSFE_Python_Discord as BCSFE_Python
 
 if __name__ == "__main__":
 	pass
